b74234f94f5c8b97bb59b4beeaa4d34d0f3d8968
jvodoor <jvodoor@gmail.com>
2023-03-02 14:03:40 -0600

    Combines original split datasets into one

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,35 +1,21 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 import pandas as pd
 
-# this goes in main as arguments: '''input_filepath, output_filepath'''
-#the 3 below just need to be uncommented, don't move them
-'''@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())'''
-def main():
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    data = pd.read_csv("C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/DATA-4950-CAPSTONE/data/external/train.csv")
+def load_dataset(file_path):
+    return pd.read_csv(file_path, index_col = 0)
+def print_df_preliminary_contents(data):
     print(data.head())
     print(data.info(verbose = 0))
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def merge_data (df1, df2):
+    df_comb = pd.concat([df1, df2], axis = 0)
+    return df_comb
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+#df_old_train = load_dataset("C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/data/external/train.csv")
+#df_old_test = load_dataset("C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/data/external/test.csv")
+#print_df_preliminary_contents(df_old_train)
+#print_df_preliminary_contents(df_old_test)
+#df = merge_data(df_old_train,df_old_test)
+#print_df_preliminary_contents(df)
+#df = df.dropna()
+#print_df_preliminary_contents(df)
+#df.to_csv("C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/data/external/comb df clean.csv")
