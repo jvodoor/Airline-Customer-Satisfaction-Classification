5b4f0b9085ddfb086459f44c7272b3ac44b198dc
jvodoor <jvodoor@gmail.com>
2023-03-03 00:33:11 -0600

    Initial Data EDA phase

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import sys
-sys.path.insert(0, '/Users/jvodo/DATA 4950/DATA-4950-Capstone/src/data')
+sys.path.insert(0, '/Users/jvodo/DATA 4950/DATA-4950-Capstone/src/data/')
 import make_dataset as md
 
+#categoricals: Gender, Customer Type, Type of Travel, Class, Inflight Distance - Cleanliness
+#Gender, Customer Type, Type of Travel, Satisfcation (y)
+
+#Function which creates ordinal encoded columns to be used on a categorical column. Each column will have a 1-z value representation,
+#where z = the number of unique datapoints per column.
 def df_encoding(df, column_name):
     list_uniques = df[column_name].unique().tolist()
     num_uniques = len(df[column_name].unique())
@@ -15,14 +20,16 @@ def df_encoding(df, column_name):
     df = df.drop(column_name, axis=1)
     return df
 
-#categoricals: Gender, Customer Type, Type of Travel, Class, Inflight Distance - Cleanliness
-#Gender, Customer Type, Type of Travel, Satisfcation (y) = binary encoding
-'''df_feat_eng = md.load_dataset("C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/data/external/comb df clean.csv")
-#md.print_df_preliminary_contents(df_feat_eng)
+
+#not quite sure yet how to adapt this so when people try the code for thesmelves they'll be able to open the dataset without needing to change the filepath.
+df_feat_eng = md.load_dataset("C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/data/external/comb df clean.csv")
+
 x, y = md.x_y_split(df_feat_eng, -1)
-#categoricals: Gender, Customer Type, Type of Travel, Class, Inflight Distance - Cleanliness
-#Gender, Customer Type, Type of Travel, Satisfcation (y) = binary encoding
-# Class = ordinal number encoding
 df_feat_eng = df_encoding(df_feat_eng, "Gender")
 print(df_feat_eng.head())
-print(df_feat_eng.info())'''
+print(df_feat_eng.info())
+
+
+
+#to be done: finish encoding our categoricals, eliminate Customer Type (disloyal/loyal), eliminate either departure or arrival delay time, potentially remove all 0 values from our survey data.
+#evaluate if this doesn't dilute our datapoints too much
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import sys
-sys.path.insert(0, '/Users/jvodo/DATA 4950/DATA-4950-Capstone/src/data')
+sys.path.insert(0, '/Users/jvodo/DATA 4950/DATA-4950-Capstone/src/data/')
 import make_dataset as md
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,6 +8,9 @@ import seaborn as sns
 df_vis = md.load_dataset("C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/data/external/comb df clean.csv")
 md.print_df_preliminary_contents(df_vis)
 x, y = md.x_y_split(df_vis, -1)
+
+
+#plots for our satisfaction surveys, the ones where we have a 0-5 score
 
 plt.figure(figsize = (16,8))
 plt.subplot(131)
@@ -63,6 +66,54 @@ plt.close()
 #total counts, plenty of data to build a consensus.
 
 
+#Customer Type, Age, Type of Travel, Class, Flight Distance, Departure Delay in Minutes,
+#Arrival delay in minutes, satisfaction
+
+df_vis.describe()
+plt.figure(figsize = (16,8))
+plt.subplot(131)
+sns.countplot(data=df_vis, x='Gender')
+plt.subplot(132)
+sns.countplot(data=df_vis, x='Customer Type')
+plt.subplot(133)
+sns.countplot(data=df_vis, x='Type of Travel')
+plt.show()
+plt.close()
+
+
+plt.figure(figsize = (16,8))
+plt.subplot(131)
+sns.countplot(data=df_vis, x='Class')
+plt.subplot(132)
+sns.countplot(data=y, x='satisfaction')
+plt.show()
+plt.close()
+
+#We seem to be skewing towards business travelers with our customer counts, though
+#there are a fair amount of economy passengers. Gender is fairly even, while most of our
+#customers would be considered "loyal customers". I may exclude loya/disloyal because
+#it feels too vague to be useful. With eco plus' low counts, I may opt to combine it
+#with eco, or just let it go as its own and see what happens.
+
+
+#heat map of numerical values
+fig = plt.figure(figsize= (8,6))
+sns.heatmap(df_vis[['Age', 'Flight Distance', 'Departure Delay in Minutes', 'Arrival Delay in Minutes']].corr(), cmap="viridis", annot=True)
+plt.show()
+#looking at our correlated numerical variables, unsurprisingly there's a strong correlation
+#between Departure and Arrival Delays. Might be worth exploring those 2 some more and decide
+#to keep 1 or the other. 
+
+
+#exploring Arrival and Departure delay in more detail
+print(df_vis['Departure Delay in Minutes' ].describe())
+print(df_vis['Arrival Delay in Minutes'].describe())
+print("")
+print("non zero Departure delays", df_vis['Departure Delay in Minutes'].astype(bool).sum(axis=0))
+print("")
+print("non zero arrival delays", df_vis['Arrival Delay in Minutes'].astype(bool).sum(axis=0))
+
+#the numbers are very similar to each other. May try 2 options where I keep both in or remove 1 or both possibly
 
 
 
@@ -74,10 +125,8 @@ plt.close()
 
 
 
-
-
-
-# This is exploring how to make a sequence of scrollable graphs, needs more exploration
+# This is exploring how to make a sequence of scrollable graphs, doesn't work yet, need
+#to do more research.
 '''
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
