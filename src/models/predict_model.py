64ccb57864008d4ea255872d80369b2f679b9428
jvodoor <jvodoor@gmail.com>
2023-04-25 01:14:12 -0500

    Final Changes

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,21 +1,17 @@
 import sys
-#sys.path = ['c:\\Users\\jvodo\\DATA 4950\\DATA-4950-Capstone\\src', 'c:\\Program Files\\Python311\\python311.zip', 'c:\\Program Files\\Python311\\DLLs', 'c:\\Program Files\\Python311\\Lib', 'c:\\Program Files\\Python311', '', 'C:\\Users\\jvodo\\AppData\\Roaming\\Python\\Python311\\site-packages', 'C:\\Users\\jvodo\\AppData\\Roaming\\Python\\Python311\\site-packages\\win32', 'C:\\Users\\jvodo\\AppData\\Roaming\\Python\\Python311\\site-packages\\win32\\lib', 'C:\\Users\\jvodo\\AppData\\Roaming\\Python\\Python311\\site-packages\\Pythonwin', 'c:\\Program Files\\Python311\\Lib\\site-packages', 'c:\\Users\\jvodo\\DATA 4950\\DATA-4950-Capstone\\src\\data', 'c:\\Users\\jvodo\\DATA 4950\\DATA-4950-Capstone\\src\\features', 'c:\\Users\\jvodo\\DATA 4950\\DATA-4950-Capstone\\src\\models']
+#sys.path = ['c:\\Users\\jvodo\\DATA 4950\\DATA-4950-Capstone\\src', 'c:\\Program Files\\Python311\\python311.zip', 'c:\\Program Files\\Python311\\DLLs', 'c:\\Program Files\\Python311\\Lib', 'c:\\Program Files\\Python311', '', 'C:\\Users\\jvodo\\AppData\\Roaming\\Python\\Python311\\site-packages', 'C:\\Users\\jvodo\\AppData\\Roaming\\Python\\Python311\\site-packages\\win32', 'C:\\Users\\jvodo\\AppData\\Roaming\\Python\\Python311\\site-packages\\win32\\lib', 'C:\\Users\\jvodo\\AppData\\Roaming\\Python\\Python311\\site-packages\\Pythonwin', 'c:\\Program Files\\Python311\\Lib\\site-packages', 'c:\\Users\\jvodo\\DATA 4950\\DATA-4950-Capstone\\src\\data', 'c:\\Users\\jvodo\\DATA 4950\\DATA-4950-Capstone\\src\\features', 'c:\\Users\\jvodo\\DATA 4950\\DATA-4950-Capstone\\src\\models', 'c:\\Users\\jvodo\\DATA 4950\\DATA-4950-Capstone\\models']
 import make_dataset as md
 import pandas as pd
 import numpy as np
-import sklearn
 from sklearn.model_selection import train_test_split
-import statsmodels
-import statsmodels.api as sm
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, roc_curve, roc_auc_score, precision_score, recall_score
-from lazypredict.Supervised import LazyClassifier
-from lazypredict.Supervised import LazyRegressor
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_curve, roc_auc_score
+
 import matplotlib.pyplot as plt
 import train_model as tm
-import pickle
+import pickle as pkl
 
-def log_reg_test_data_statistics(logistic, X_test, y_test):
+def log_reg_test_data_statistics(logistic, X_test, y_test, model):
 
     log_pred_test = logistic.predict(X_test)
 
@@ -23,16 +19,15 @@ def log_reg_test_data_statistics(logistic, X_test, y_test):
     logexport_prob = logistic.predict_proba(X_test) 
 
     #print confusion matrix for testing data
-    print("Logistic Regression Model Testing Data Confusion Matrix :")
+    print(model, " Model Testing Data Confusion Matrix :")
     print(confusion_matrix(y_test, log_pred_test)) 
 
     #scores
     accuracy_testing = logistic.score(X_test, y_test)
-    precision_test = precision_score(y_test, log_pred_test)
-    recall_test = recall_score(y_test, log_pred_test)
     print('\n')
-    print("Recall: ", recall_test)
-    print("Precision: ", precision_test)
+
+    print (model, "Model performance scores: ", "\n")
+    
     print('Accuracy:', accuracy_testing.round(3))
 
 
@@ -58,22 +53,27 @@ def log_reg_test_data_statistics(logistic, X_test, y_test):
 
 
 #load data set
-df_test = md.load_dataset("/data/processed/df alt no gender.csv")
+df_final_log_data = md.load_dataset("C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/data/processed/final data log model.csv")
+df_final_tree_data = md.load_dataset("C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/data/processed/final data tree model.csv")
 
 #split data into Y target and X predictors
-X, y = md.x_y_split(df_test, -1)
+log_X, log_y = md.x_y_split(df_final_log_data, -1)
+tree_X, tree_y = md.x_y_split(df_final_tree_data, -1)
+
 
 #80/20 train test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 21)
+log_X_train, log_X_test, log_y_train, log_y_test = train_test_split(log_X, log_y, test_size = 0.2, random_state = 21)
+tree_X_train, tree_X_test, tree_y_train, tree_y_test = train_test_split(tree_X, tree_y, test_size = 0.2, random_state = 21)
 
-#logistic regression training
-logistic_model = tm.log_or_boost_train(X_train, X_test, y_train, 'log')
+#load picked models
+logistic_model = pkl.load(open('C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/models/logistic_train.pkl', 'rb'))
+tree_model = pkl.load(open('C:/Users/jvodo/DATA 4950/DATA-4950-Capstone/models/xg_train.pkl', 'rb'))
+
 #logistic regression test statistics
-log_reg_test_data_statistics(logistic_model, X_test, y_test) 
+log_reg_test_data_statistics(logistic_model, log_X_test, log_y_test, "Logistic Regression") 
 
-#boosted tree training
-tree_model = tm.log_or_boost_train(X_train, X_test, y_train, 'boost')
+print("----------------------------------------------------------------------------", "\n")
 #boosted tree statistics
-log_reg_test_data_statistics(tree_model, X_test, y_test) 
+log_reg_test_data_statistics(tree_model, tree_X_test, tree_y_test, "XGBoosted Tree") 
 
 
